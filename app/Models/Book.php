a0455da0a51f2d0e52851fc8b7e7aa7409441d76
MohamedSalamaMouse <slamtm608@gmail.com>
2023-08-17 00:04:44 +0300

    Dashboard and Migration

---
+++ app/Models/Book.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Book extends Model
+{
+    use HasFactory;
+    use softDeletes;
+    protected $fillable = [
+        'title',
+        'slug',
+        'description',
+        'cover',
+        'price',
+        'author_id',
+        'category_id',
+        'borrow',
+    ];
+
+}
