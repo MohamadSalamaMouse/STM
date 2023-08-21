5b7849b355239f28ba5e395fa8a9df67f0a5b728
MohamedSalamaMouse <slamtm608@gmail.com>
2023-08-22 02:54:27 +0300

    edit

--- resources/views/admin/layouts/main-sidebar.blade.php
+++ resources/views/admin/layouts/main-sidebar.blade.php
@@ -1,50 +1,29 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-        <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
-    </a>
+
 
     <!-- Sidebar -->
     <div class="sidebar">
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
-                <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-            </div>
+
             <div class="info">
-                <a href="#" class="d-block">Alexander Pierce</a>
+                <a href="#" class="d-block">{{Auth::user()->name}}</a>
             </div>
         </div>
 
         <!-- SidebarSearch Form -->
-        <form class="form-inline" action="{{ route('Admin.search') }}" method="POST">
-            @csrf
+        <div class="form-inline">
             <div class="input-group" data-widget="sidebar-search">
-                <input class="form-control form-control-sidebar" type="search" placeholder="Search student by id" aria-label="Search" name="search">
+                <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
                 <div class="input-group-append">
-                    <button class="btn btn-sidebar" type="submit">
+                    <button class="btn btn-sidebar">
                         <i class="fas fa-search fa-fw"></i>
                     </button>
                 </div>
             </div>
-        </form>
-
-        @if(isset($results))
-            <div>
-                <h2>Search Results</h2>
-                @if($results->isEmpty())
-                    <p>No results found.</p>
-                @else
-                    <ul>
-                        @foreach($results as $result)
-                            <li>{{ $result->name }}</li>
-                        @endforeach
-                    </ul>
-                @endif
-            </div>
-        @endif
+        </div>
 
         <!-- Sidebar Menu -->
         <nav class="mt-2">
@@ -75,6 +54,13 @@
                             <i class="fas fa-angle-left right"></i>
                         </p>
                     </a>
+                    <a href="{{route('Admin.book.borrow')}}" class="nav-link">
+                        <i class="nav-icon fas fa-table"></i>
+                        <p>
+                            Book Borrow
+                            <i class="fas fa-angle-left right"></i>
+                        </p>
+                    </a>
                     <a href="{{route('Admin.users')}}" class="nav-link">
                         <i class="nav-icon fas fa-table"></i>
                         <p>
@@ -82,7 +68,6 @@
                             <i class="fas fa-angle-left right"></i>
                         </p>
                     </a>
-
             </ul>
 
                 </li>
